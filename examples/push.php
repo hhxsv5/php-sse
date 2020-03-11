1ef917a573d5854a8126c7cad947cc1c5dae4a09
谢彪 <hhxsv5@sina.com>
2020-03-11 10:33:30 +0800

    optimize docs

--- examples/push.php
+++ examples/push.php
@@ -4,24 +4,24 @@ include '../vendor/autoload.php';
 use Hhxsv5\SSE\SSE;
 use Hhxsv5\SSE\Update;
 
-//example: push messages to client
+// Example: push messages to client
 
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
-header('X-Accel-Buffering: no');//Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
+header('X-Accel-Buffering: no'); // Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
 
 (new SSE())->start(new Update(function () {
     $id = mt_rand(1, 1000);
-    $newMsgs = [
+    $news = [
         [
             'id'      => $id,
-            'title'   => 'title' . $id,
-            'content' => 'content' . $id,
+            'title'   => 'title #' . $id,
+            'content' => 'content #' . $id,
         ],
-    ];//get data from database or service.
-    if (!empty($newMsgs)) {
-        return json_encode(['newMsgs' => $newMsgs]);
+    ]; // Get news from database or service.
+    if (!empty($news)) {
+        return json_encode(['news' => $news]);
     }
-    return false;//return false if no new messages
-}), 'new-msgs');
+    return false;// Return false if no new messages
+}), 'news');
